02f23368d6fc090a99eeff278eb72da0a1dd31ee
Lakshay-0 <itslakshayjain@gmail.com>
2023-05-14 23:43:35 +0530

    payment gateway removed

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const path = require('path')
 const cors = require('cors')
-const shortid = require('shortid')
-const Razorpay = require('razorpay')
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 
@@ -23,38 +20,7 @@ const corsOptions = {
     credentials: true
 }
 
-const razorpay = new Razorpay({
-    key_id: "rzp_test_0HNVfUdXU1ot3f",
-    key_secret: "1pLh9TstRmp1KOSSljY1YItE",
-});
 
-app.use(cors())
-app.get('/User.png', (req,res)=>{
-    res.sendFile(path.join(__dirname,"User.png"))
-})
-
-app.post('/razorpay', async (req,res) => {
-    const payment_capture = 1
-    const amount = 499
-    const currency = 'INR'
-    const options = {
-        amount: amount*100,
-        currency: currency,
-        receipt: shortid.generate(),  //this will generate random receipt.
-        payment_capture
-    }
-    try{
-        const response = await razorpay.orders.create(options)
-        console.log(response)
-        res.json({
-            id: response.id,
-            currency: response.currency,
-            amount: response.amount
-        })
-    } catch(error){
-        console.log(error)
-    }
-})
 
 // connecting database
 mongoose.set('strictQuery', false);
@@ -71,7 +37,7 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
-//app.options('/api/v1/auth/login', cors());
+
 
 // routes path and redirecting...
 const routePath = '/api/v1';
--- frontend/src/components/Booking/Booking.jsx
+++ frontend/src/components/Booking/Booking.jsx
@@ -48,7 +48,6 @@ const Booking = ({ tour, avgRating }) => {
             if(!user || user === undefined || user === null) {
                 return alert('Please Sign in');
             }
-            console.log(booking)
             const res = await fetch(`${BASE_URL}/booking`, {
                 method: 'POST',
                 headers: {
@@ -67,30 +66,6 @@ const Booking = ({ tour, avgRating }) => {
         } catch (error) {
             alert(error.message);
         }
-
-        const data = await fetch("http://localhost:4000/razorpay",{
-            method: 'POST'
-        }).then((t) => t.json())
-
-        const options = {
-            key: "rzp_test_0HNVfUdXU1ot3f",
-            currency: data.currency,
-            amount: data.amount,
-            description: 'Wallet Transaction',
-            image: 'http://localhost:4000/User.png',
-            order_id: data.id,
-            handler: function(response){
-                alert("PAYMENT ID: " + response.razorpay_payment_id)
-                alert("ORDER ID: " + response.razorpay_order_id)
-            },
-            prefill: {
-                name: 'GAUTAM SHARMA',
-                email: 'geekygautam1997@gmail.com',
-                contact: '9418306776'
-            }
-        };
-        const paymentObject = new window.Razorpay(options)
-        paymentObject.open()
 
     }
 
